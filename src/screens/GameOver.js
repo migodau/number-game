b296db1475fa11ab5e878facd45b1375a412835c
migodau <migodau@gmail.com>
2022-12-29 22:15:59 -0300

    styling game over screen

--- src/screens/GameOver.js
+++ src/screens/GameOver.js
@@ -1,14 +1,21 @@
-import { StyleSheet, View, Text } from 'react-native'
+import { StyleSheet, View, Text, Image } from 'react-native'
 import Button from '../components/ui/Button'
 import Title from '../components/ui/Title'
 import { COLORS } from '../theme/colors'
 
-export default GameOver = ({ onRestartGame }) => {
+export default GameOver = ({ guessedNumber, onRestartGame }) => {
   return (
     <View style={styles.container}>
+      
       <View style={styles.gameOverContainer}>
+        <View style={styles.imageContainer}>
+          <View style={styles.imageWrapper}>
+            <Image style={styles.image} source={require('../assets/images/welldone_cat.jpg')} />
+          </View>
+        </View>
         <View style={styles.textContainer}>
           <Title>✨ Yay!! ✨</Title>
+          <Text>The number is {guessedNumber}!!</Text>
           <Text>Thank you for playing!</Text>
         </View>
         <View style={styles.buttonWrapper}>
@@ -26,8 +33,23 @@ const styles = StyleSheet.create({
     marginTop: 110,
     alignItems: 'stretch',
   },
+  imageContainer: {
+    alignItems: 'center',
+  },
+  imageWrapper: {
+    width: 150,
+    height: 150,
+    overflow: 'hidden',
+    borderRadius: 20,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
   gameOverContainer: {
-    backgroundColor: COLORS.background700,
+    backgroundColor: '#fff',
+    borderColor: COLORS.background500,
+    borderWidth: 10,
     paddingVertical: 24,
     marginHorizontal: 24,
     borderRadius: 12,
